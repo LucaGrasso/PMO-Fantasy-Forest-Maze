edc370212fff33d674397fdd1778a60fbb77bfe9
Luca Grasso su Ubuntu <lucagrasso@navert.it>
2024-01-15 17:50:45 +0100

    WIP<Debug> Debug Maven funzionante, studiare come lanciarlo insieme al JavaFX

--- src/main/java/it/lucagrasso/Main.java
+++ src/main/java/it/lucagrasso/Main.java
@@ -1,36 +1,28 @@
 package it.lucagrasso;
 
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import it.lucagrasso.views.controller.SceneController;
 import it.lucagrasso.utilities.ExceptionLogger;
 
 
-
-
 // https://edencoding.com/mvc-in-javafx/
-
 // Abstract of javafx.application.Application
 // The entry point for JavaFX applications is the Application class:
 // ref: https://openjfx.io/javadoc/11/javafx.graphics/javafx/application/Application.html
 public class Main extends Application {
-
     public static void main(String[] args) {
-    System.out.println("Applicazione in inizializzazione");
+        System.out.println("Applicazione in inizializzazione");
         System.out.println("Test del main");
-//        launch(args);
+        launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) {
         try {
             new SceneController("01_LoginNew", primaryStage, 800, 600);
         } catch (Exception e) {
             ExceptionLogger logger = ExceptionLogger.getInstance();
-
             logger.logException("Errore nell`apertura di 01_login", e);
-
         }
     }
 }
--- src/main/java/it/lucagrasso/views/controller/SceneController.java
+++ src/main/java/it/lucagrasso/views/controller/SceneController.java
@@ -1,5 +1,6 @@
 package it.lucagrasso.views.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,6 +9,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author: Luca Grasso
@@ -20,58 +23,37 @@ public class SceneController {
     private final int width;
     private final int height;
 
-    public SceneController(String nameScene, Stage stage, int width, int height) {
+    private static final Logger LOGGER = Logger.getLogger(SceneController.class.getName());
 
-        this.nameScene = "/views/fxml/" + nameScene + ".fxml";
+    public SceneController(String nameScene, Stage stage, int width, int height) {
+        this.nameScene = "views/fxml/" + nameScene + ".fxml";
         this.primaryStage = stage;
         this.width = width;
         this.height = height;
         switchToCreation();
     }
-
-    //
     private void switchToCreation() {
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(nameScene)));
 
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(nameScene)));
             primaryStage.setTitle("Enchanted Woods");
             primaryStage.setScene(new Scene(root, width, height));
             primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/resources/img/tree1.png"))));
             primaryStage.setResizable(false);
             primaryStage.show();
-        }catch (Exception e){
-            // potentially add error logging here
-            // navigate to an error scene or display an error dialog
-            // return some value to signal the error
+        } catch (Exception e){
+            LOGGER.log(Level.SEVERE, "An error occurred while switching scenes.", e);
         }
     }
-
     public void switchToScene(String nameScene, Stage stage, int width, int height) throws IOException {
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/application/views/fxml/" + nameScene + ".fxml")));
-
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/fxml/" + nameScene + ".fxml")));
         Scene scene = new Scene(root, width, height);
         stage.setScene(scene);
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/resources/img/tree1.png"))));
         stage.setResizable(false);
         stage.show();
     }
-
-
-
-
-
-
-
     public void switchToScene(String nameScene, Stage stage) throws IOException {
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/application/views/fxml/" + nameScene + ".fxml")));
-
-        Scene scene = new Scene(root, 800,600);
-        stage.setScene(scene);
-        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/resources/img/tree1.png"))));
-        stage.setResizable(false);
-        stage.show();
+        switchToScene(nameScene, stage, 800, 600);
     }
-
 }
