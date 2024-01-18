9d1d99b64090a722be16bdc1a200e9132c2baffd
LGrasso su macbook <luca.grasso80@gmail.com>
2024-01-18 21:07:03 +0100

    WIP<Database> riorganizzato l'approggio della connesione con un file fisico

--- src/main/java/it/lucagrasso/models/DatabaseConfig.java
+++ src/main/java/it/lucagrasso/models/DatabaseConfig.java
@@ -1,7 +1,34 @@
 package it.lucagrasso.models;
 
-public abstract class DatabaseConfig {
-    public abstract String getUrl();
-    public abstract String getUser();
-    public abstract String getPassword();
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DatabaseConfig {
+
+    private final Properties properties;
+
+    public DatabaseConfig() {
+        properties = new Properties();
+    }
+
+    protected void load(String propertiesFilename) {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(propertiesFilename)) {
+            properties.load(input);
+        } catch (IOException e) {
+            // handle the exception
+        }
+    }
+
+    public String getUrl() {
+        return properties.getProperty("DB_URL");
+    }
+
+    public String getUser() {
+        return properties.getProperty("DB_USER");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("DB_PASSWORD");
+    }
 }
